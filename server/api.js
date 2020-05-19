dd8fed6ac35df0f03a0a846506e2e21e198e0783
Elena Hernandez Vazquez <selenelunaw@hotmail.com>
2020-05-19 13:36:53 -0500

    ready to go

--- server/api.js
+++ server/api.js
@@ -3,10 +3,8 @@ const apiRouter = express.Router();
 
 const booksRouter = require('./books');
 const listsRouter = require('./lists');
-const filterRouter = require('./filter')
 
 apiRouter.use('/books', booksRouter);
 apiRouter.use('/lists', listsRouter);
-//apiRouter.use('/filter', filterRouter);
 
 module.exports = apiRouter;
